75cb93c442bbfd5a08b15651d64fd9a917b88d1a
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-05-24 15:34:05 +0300

    OOP

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title ='Goodness', page_count ='0'):
+
+        self.page_count = page_count
+        self.title = title
+
+    @property    
+    def page_count(self):
+        return self._page_count 
+
+    @page_count.setter 
+    def page_count(self, page_count):
+        if isinstance(page_count, str):
+            self._page_count = page_count
+        else:
+            raise ValueError (
+                 "page_count must be an integer"
+                 )
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        print ("Flipping the page...wow, you read fast!")
+Book()
+
     
         
